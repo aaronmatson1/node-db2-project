48d6cd5188c43e5824da96ff94e80f16305e91cb
aaronmatson1 <matsonaaron1@gmail.com>
2020-05-26 19:49:56 -0600

    Initial Setup

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+module.exports = {
+    development: {
+      client: "sqlite3",
+      connection: {
+        filename: "./data/db.sqlite3"
+      },
+      migrations: {
+        directory: "./data/migrations"
+      },
+      seeds: {
+        directory: "./data/seeds"
+      }
+    }
+  };
